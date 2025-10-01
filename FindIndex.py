b8c533e732b7cf94c44186d46ea214a1be54aa0f
mipka <111595895+NaynTac@users.noreply.github.com>
2025-10-01 11:53:46 +0300

    Create FindIndex.py

---
+++ FindIndex.py
@@ -0,0 +1,8 @@
+def find_index(nums, target):
+    for i in range(len(nums)):
+        try:
+            j = nums[i + 1:].index(target - nums[i]) + i    # Находим второй элемент пары
+            return [i, j + 1]
+        except:
+            continue
+    return -1   # Если пара не найдется, возвращаем -1
