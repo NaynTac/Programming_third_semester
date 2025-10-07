83270e4f03ae81e80d892ba429c09d3763b4c70f
mipka <111595895+NaynTac@users.noreply.github.com>
2025-10-07 19:22:53 +0300

    Update FindIndexTest.py

--- ЛР №2/Код/FindIndexTest.py
+++ ЛР №2/Код/FindIndexTest.py
@@ -8,11 +8,8 @@ if __name__ == "__main__":
     
         def test_with_target(self):
             self.assertEqual(find_index([2, 7, 11, 15], 9), [0, 1])
-            self.assertEqual(find_index([3, 2, 4], 6), [1, 2])
-            self.assertEqual(find_index([3, 3], 6), [0, 1])
         
         def test_without_target(self):
             self.assertEqual(find_index([2, 7, 11, 15], 11), -1)
-            self.assertEqual(find_index([3, 2, 4], 8), -1)
         
     unittest.main()
